ff80781943e3b6395101081d1944f5fbf7ef073b
Dan Hammond <dan.hammond@education.co.uk>
2022-08-25 15:05:17 +0100

    initial commit

---
+++ TestWebsite/App_Plugins/TestPackage/controller.js
@@ -0,0 +1,20 @@
+﻿angular.module('umbraco').controller('TestPropertyEditorController', function ( $scope ) {
+  if ( !$scope.model.value || typeof $scope.model.value !== 'object' ) {
+    $scope.model.value = {
+      boolProp: false,
+      otherBoolProp: true,
+      numProp: 1,
+      otherNumProp: 0.5,
+      stringProp: "test",
+      otherStringProp: "",
+      objectProp: {
+        nestedBoolProp: false,
+        nestedOtherBoolProp: true,
+        nestedNumProp: 1,
+        nestedOtherNumProp: 0.5,
+        nestedStringProp: "test",
+        nestedOtherStringProp: ""
+      }
+    }
+  }
+});
